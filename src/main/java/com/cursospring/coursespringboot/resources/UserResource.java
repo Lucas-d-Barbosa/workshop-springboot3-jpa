29c80dca9d31ebcab0fe8b12c6b8f068c69baafb
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-02-21 20:06:49 -0300

    User entity and resource

---
+++ src/main/java/com/cursospring/coursespringboot/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.cursospring.coursespringboot.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursospring.coursespringboot.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Lucas", "lucas@gmail.com", "999900", "223");
+		return ResponseEntity.ok().body(u);
+	}
+}
